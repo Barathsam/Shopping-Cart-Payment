5a4f4e83764bd52b7f01858e2f58c1b3f7926c0c
barath sam <barathsam555.p@gmail.com>
2022-03-17 18:52:58 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,65 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+
+const stripeSecretKey =process.env.STRIPE_SECRET_KEY
+const stripePublicKey =process.env.STRIPE_PUBLIC_KEY
+
+console.log(stripePublicKey)
+
+const express = require('express')
+const app = express()
+const cors= require('cors')
+const fs = require('fs')
+const stripe = require('stripe')(stripeSecretKey)
+
+app.use(cors())
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/store', function (req, res) {
+    fs.readFile('items.json', function (err,data) {
+        if(err){
+            res.status(500).end()
+        }else{
+            res.render('store.ejs', {
+                stripePublicKey:stripePublicKey,
+                items: JSON.parse(data)
+            })
+        }
+    })
+})
+
+
+app.post('/purchase', function (req, res) {
+    fs.readFile('items.json', function (err,data) {
+        if(err){
+            res.status(500).end()
+        }else{
+            const itemsJson = JSON.parse(data)
+            const itemsArray = itemsJson.music.concat(itemsJson.merch)
+            let total = 0
+            req.body.items.forEach(function(item){
+                const itemJson= itemsArray.find(function(i){
+                    return i.id== item.id
+                })
+                total = total + itemJson.price * item.quantity
+            })
+            stripe.charges.create({
+                amount:total,
+                scource:req.body.stripeTokenId,
+                currency:'usd'
+            }).then(function(){
+                console.log('charge successful');
+                res.json({message:'successful purchased items' })
+            }).catch(function(){
+                console.log('charge Failed');
+                res.status(500).end();
+            })
+        }
+    })
+})
+
+app.listen(5000)
